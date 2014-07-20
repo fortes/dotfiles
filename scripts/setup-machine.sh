6b228ce3e25cd106ca1996734e56deb577fdb85a
Filipe Fortes <accounts@fortes.com>
2014-07-19 17:52:08 -0700

    Fix linux install script

--- scripts/setup-machine.sh
+++ scripts/setup-machine.sh
@@ -59,7 +59,7 @@ elif [ $OS == "Linux" ]; then
   fi
 
   if [ ! $HEADLESS ]; then
-    if [ ! -f /etc/apt/sources.list.d/google.list ]; then
+    if [ ! -f /etc/apt/sources.list.d/google-chrome.list ]; then
       echo "Adding Chrome PPA (requires sudo)"
       wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
       sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
