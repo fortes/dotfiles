3754cab51ae57df2af57354577abfe6bba91cad2
Filipe Fortes <accounts@fortes.com>
2014-08-10 17:53:51 -0700

    Make sure to run apt-get update before trying to install

--- scripts/intel-nuc-setup.sh
+++ scripts/intel-nuc-setup.sh
@@ -2,6 +2,7 @@
 if [ ! -f /etc/apt/sources.list.d/ubuntu-audio-dev-alsa-daily-trusty.list ]; then
   echo "Adding alsa-daily apt source (requires sudo)"
   sudo add-apt-repository -y ppa:ubuntu-audio-dev/alsa-daily
+  sudo apt-get -q update
   sudo -E apt-get -qfuy install oem-audio-hda-daily-dkms
 fi
 
