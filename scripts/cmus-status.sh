6f5a320156ec8122b49a5efc4719bbb22440a96c
Filipe Fortes <accounts@fortes.com>
2015-03-31 15:16:53 -0700

    Have cmus show song playing in tmux status

---
+++ scripts/cmus-status.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Report to last.fm
+# Run last-cmus in background so we don't delay tmux status message
+python ~/dotfiles/scripts/last-cmus.py "$@" &
+
+# Send message to tmux
+if [ -n $TMUX ]; then
+  title=""
+  album=""
+  artist=""
+  status="stopped"
+
+  # Parse out the cmus arguments, based off of:
+  # https://github.com/cmus/cmus/wiki/status_display_short_text.sh
+  #
+  # Output looks like:
+  # status playing file /path/to/file.mp3 artist ArtistName ablum AlbumName ...
+  while [ "$1" != "" ]; do
+    case "$1" in
+      title)
+        title="$2"
+      ;;
+      album)
+        album="$2"
+      ;;
+      artist)
+        artist="$2"
+      ;;
+      status)
+        status="$2"
+      ;;
+      *)
+      ;;
+    esac
+    shift
+    shift
+  done
+
+  if [ "$status" = "stopped" ]; then
+    message="cmus stopped"
+  elif [ "$status" = "paused" ]; then
+    message="cmus paused"
+  elif [ -n "$title" ] && [ -n "$artist" ]; then
+    # Make the message display long enough to be seen
+    tmux set-opt display-time 3000
+    message="$artist - $title"
+  fi
+
+  if [ -n "$message" ]; then
+    tmux display-message "♬ $message"
+    # Reset display-time
+    tmux set-opt -u display-time
+  fi
+fi
