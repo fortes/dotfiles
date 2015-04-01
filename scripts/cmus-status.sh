b529a6963410db9240949ad8855fdf244ced4dbd
Filipe Fortes <accounts@fortes.com>
2015-03-31 19:55:51 -0700

    Update tmux/cmus status

--- scripts/cmus-status.sh
+++ scripts/cmus-status.sh
@@ -5,55 +5,56 @@
 python ~/dotfiles/scripts/last-cmus.py "$@" &
 
 # Send message to tmux (disabled since already in status bar)
-#if [ -n $TMUX ]; then
-if false; then
-  title=""
-  album=""
-  artist=""
-  status="stopped"
+if [ -n $TMUX ]; then
+  if false; then
+    title=""
+    album=""
+    artist=""
+    status="stopped"
 
-  # Parse out the cmus arguments, based off of:
-  # https://github.com/cmus/cmus/wiki/status_display_short_text.sh
-  #
-  # Output looks like:
-  # status playing file /path/to/file.mp3 artist ArtistName ablum AlbumName ...
-  while [ "$1" != "" ]; do
-    case "$1" in
-      title)
-        title="$2"
-      ;;
-      album)
-        album="$2"
-      ;;
-      artist)
-        artist="$2"
-      ;;
-      status)
-        status="$2"
-      ;;
-      *)
-      ;;
-    esac
-    shift
-    shift
-  done
+    # Parse out the cmus arguments, based off of:
+    # https://github.com/cmus/cmus/wiki/status_display_short_text.sh
+    #
+    # Output looks like:
+    # status playing file /path/to/file.mp3 artist ArtistName ablum AlbumName ...
+    while [ "$1" != "" ]; do
+      case "$1" in
+        title)
+          title="$2"
+        ;;
+        album)
+          album="$2"
+        ;;
+        artist)
+          artist="$2"
+        ;;
+        status)
+          status="$2"
+        ;;
+        *)
+        ;;
+      esac
+      shift
+      shift
+    done
 
-  if [ "$status" = "stopped" ]; then
-    message="cmus stopped"
-  elif [ "$status" = "paused" ]; then
-    message="cmus paused"
-  elif [ -n "$title" ] && [ -n "$artist" ]; then
-    # Make the message display long enough to be seen
-    tmux set-opt display-time 3000
-    message="$artist - $title"
+    if [ "$status" = "stopped" ]; then
+      message="cmus stopped"
+    elif [ "$status" = "paused" ]; then
+      message="cmus paused"
+    elif [ -n "$title" ] && [ -n "$artist" ]; then
+      # Make the message display long enough to be seen
+      tmux set-opt display-time 3000
+      message="$artist - $title"
+    fi
+
+    if [ -n "$message" ]; then
+      tmux display-message "♬ $message"
+      # Reset display-time
+      tmux set-opt -u display-time
+    fi
   fi
 
-  if [ -n "$message" ]; then
-    tmux display-message "♬ $message"
-    # Reset display-time
-    tmux set-opt -u display-time
-  fi
+  # Manually refresh the status bar
+  tmux refresh-client -S
 fi
-
-# Manually refresh the status bar
-tmux refresh-client -S
--- scripts/cmus-tmux-statusbar.sh
+++ scripts/cmus-tmux-statusbar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if cmus-remote -Q > /dev/null 2> /dev/null; then
+  STATUS+=$(cmus-remote -Q | ack status | head -n 1 | cut -d' ' -f2-)
   ARTIST+=$(cmus-remote -Q | ack 'tag artist' | head -n 1 | cut -d' ' -f3-)
   TITLE=$(cmus-remote -Q | ack 'tag title' | cut -d' ' -f3-)
   OUTPUT="$ARTIST - $TITLE"
@@ -7,6 +8,10 @@ if cmus-remote -Q > /dev/null 2> /dev/null; then
   if [ "${#OUTPUT}" -ge 50 ]; then
     OUTPUT=$TITLE
   fi
-  OUTPUT="♬ $OUTPUT"
+  if [ "$STATUS" = "playing" ]; then
+    OUTPUT="[▶ $OUTPUT]"
+  else
+    OUTPUT="[❚❚$OUTPUT]"
+  fi
 fi
 echo $OUTPUT
