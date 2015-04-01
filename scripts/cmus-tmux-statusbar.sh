2d8e4b11ca838bc7fcd7eccfd08773aa57974f29
Filipe Fortes <accounts@fortes.com>
2015-04-01 10:50:35 -0700

    Update cmus-tmux status script

--- scripts/cmus-tmux-statusbar.sh
+++ scripts/cmus-tmux-statusbar.sh
@@ -3,15 +3,21 @@ if cmus-remote -Q > /dev/null 2> /dev/null; then
   STATUS+=$(cmus-remote -Q | ack status | head -n 1 | cut -d' ' -f2-)
   ARTIST+=$(cmus-remote -Q | ack 'tag artist' | head -n 1 | cut -d' ' -f3-)
   TITLE=$(cmus-remote -Q | ack 'tag title' | cut -d' ' -f3-)
-  OUTPUT="$ARTIST - $TITLE"
-  # Only show the song title if we are over 50 characters
-  if [ "${#OUTPUT}" -ge 50 ]; then
-    OUTPUT=$TITLE
-  fi
-  if [ "$STATUS" = "playing" ]; then
-    OUTPUT="[▶ $OUTPUT]"
+  if [ -n "$TITLE" ]; then
+    OUTPUT="$ARTIST - $TITLE"
+
+    # Only show the song title if we are over 50 characters
+    if [ "${#OUTPUT}" -ge 50 ]; then
+      OUTPUT=$TITLE
+    fi
+
+    if [ "$STATUS" = "playing" ]; then
+      OUTPUT="[▶ $OUTPUT]"
+    else
+      OUTPUT="[❚❚$OUTPUT]"
+    fi
   else
-    OUTPUT="[❚❚$OUTPUT]"
+    OUTPUT=''
   fi
 fi
 echo $OUTPUT
