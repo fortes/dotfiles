faadce1b526fcada2d09cb0e11be839bd66e157e
Filipe Fortes <accounts@fortes.com>
2015-04-14 21:02:46 -0700

    Make NeoVim setup install neovim

--- scripts/nvim-setup.sh
+++ scripts/nvim-setup.sh
@@ -2,6 +2,33 @@
 set -eo pipefail
 
 DOTFILES_DIR=$HOME/dotfiles
+OS=`uname`
+
+if [ $OS == 'Darwin' ]; then
+  if [[ -z $(brew tap | grep -i neovim) ]]; then
+    echo 'Adding NeoVim tap to Homebrew'
+    brew tap neovim/homebrew-neovim
+  fi
+  if which neovim &> /dev/null; then
+    echo 'Installing latest NeoVim from HEAD'
+    #brew install --HEAD neovim
+  else
+    read -p 'NeoVim already installed. Update from HEAD [yn]? ' -n 1 -r
+    echo ''
+    if [[ $REPLY =~ [Yy]$ ]]; then
+      echo 'Updating to latest NeoVim'
+      #brew update
+      #brew reinstall --HEAD neovim
+    else
+      echo 'Skipping NeoVim update'
+    fi
+  fi
+elif [ $OS == 'Linux' ]; then
+  echo 'TOOD: Linux installation'
+  exit 1
+fi
+
+echo '✓ NeoVim installed!'
 
 if [ ! -f $HOME/.nvim/autoload/plug.vim ]; then
   echo 'Installing vim-plug for Neovim'
@@ -18,5 +45,6 @@ if ! diff $HOME/.nvimrc $HOME/dotfiles/symlinks/nvimrc &> /dev/null; then
   ln -s $HOME/dotfiles/symlinks/nvimrc $HOME/.nvimrc
 fi
 
+echo 'Installing plugins'
 nvim +PlugInstall +qall
-echo 'NeoVim setup complete'
+echo '✓ NeoVim setup complete'
