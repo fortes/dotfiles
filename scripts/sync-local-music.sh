70a6ccb6c4a45904be64600474334ec7d6894c80
Filipe Fortes <accounts@fortes.com>
2015-06-30 11:43:06 -0700

    Update music syncing script

--- scripts/sync-local-music.sh
+++ scripts/sync-local-music.sh
@@ -3,9 +3,17 @@ set -euo pipefail
 CURRENT_YEAR=2014
 LOCAL_MUSIC_DIR=~/albums
 
-# Copy only current year albums
-echo "Cleaning directory before copy..."
-dot_clean /var/music
-rsync -avzhm --progress --size-only --force --include="*\[$CURRENT_YEAR]" --include='*.mp3' --include='*.jpg' --exclude='*' --delete-after /var/music/albums/ $LOCAL_MUSIC_DIR
-echo "Generating album list..."
-beet ls -a added:"$CURRENT_YEAR" year:"$CURRENT_YEAR" -f'%time{$added, %U} $albumartist - $album ($genre)' | sort -r > $LOCAL_MUSIC_DIR/albums-"$CURRENT_YEAR".txt
+echo "Copying files"
+rsync -avzP --force --delete-after --include="*\[$YEAR]" --include="*.mp3" \
+--include="*.jpg" --exclude="*" fedia:/var/media/music/albums/ \
+$LOCAL_MUSIC_DIR/albums/$YEAR
+
+echo "Generating album list for $YEAR"
+# Generate this outside of the command since the variables won't pass through
+BEETS_COMMAND="source ~/virtualenvs/default/bin/activate; \
+beet -c ~/.beets/config.yaml ls -a year:$YEAR -f'%time{\$added, %Y-%U} \
+\$month \$albumartist - \$album (\$genre)'"
+ssh fedia $BEETS_COMMAND | sort -r > $LOCAL_MUSIC_DIR/albums/albums-"$YEAR".txt
+
+# To see output by release month (instead of added), use:
+# cut -f3- -d' ' albums-xxxx.txt | sort -r
