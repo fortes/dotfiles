9c9517b3ac04cb7a11eff6e853a1ca3a17543e26
Filipe Fortes <accounts@fortes.com>
2015-07-15 11:37:52 -0700

    Update music sync command

--- scripts/sync-local-music.sh
+++ scripts/sync-local-music.sh
@@ -1,11 +1,24 @@
-set -euo pipefail
+# Copies albums from remote server to local machine
 
-CURRENT_YEAR=2014
-LOCAL_MUSIC_DIR=~/albums
+if [ -z $REMOTE_MUSIC_PATH ]; then
+  echo "Must have REMOTE_MUSIC_PATH & REMOTE_MUSIC_SERVER variables set"
+  exit
+fi
+
+if [ -z $LOCAL_MUSIC_DIR ]; then
+  echo "Must have LOCAL_MUSIC_DIR, etc variables set"
+  exit
+fi
+
+if [ -z $1 ]; then
+  YEAR=`date +"%Y"`
+else
+  YEAR=$1
+fi
 
 echo "Copying files"
 rsync -avzP --force --delete-after --include="*\[$YEAR]" --include="*.mp3" \
---include="*.jpg" --exclude="*" fedia:/var/media/music/albums/ \
+--include="*.jpg" --exclude="*" $REMOTE_MUSIC_SERVER:$REMOTE_MUSIC_PATH \
 $LOCAL_MUSIC_DIR/albums/$YEAR
 
 echo "Generating album list for $YEAR"
@@ -13,7 +26,7 @@ echo "Generating album list for $YEAR"
 BEETS_COMMAND="source ~/virtualenvs/default/bin/activate; \
 beet -c ~/.beets/config.yaml ls -a year:$YEAR -f'%time{\$added, %Y-%U} \
 \$month \$albumartist - \$album (\$genre)'"
-ssh fedia $BEETS_COMMAND | sort -r > $LOCAL_MUSIC_DIR/albums/albums-"$YEAR".txt
+ssh $REMOTE_MUSIC_SERVER $BEETS_COMMAND | sort -r > $LOCAL_MUSIC_DIR/albums/albums-"$YEAR".txt
 
 # To see output by release month (instead of added), use:
 # cut -f3- -d' ' albums-xxxx.txt | sort -r
