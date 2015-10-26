1c88db2c2e11dadf34584472348ed944ac4aa34d
Filipe Fortes <accounts@fortes.com>
2015-10-26 14:43:42 -0700

    Fix stupid typo

--- scripts/nvim-setup.sh
+++ scripts/nvim-setup.sh
@@ -43,7 +43,7 @@ else
 fi
 
 echo "$CMARK Neovim installed"
-$NVIM_CONFIG_DIR=$XDG_CONFIG_HOME/nvim
+NVIM_CONFIG_DIR=$XDG_CONFIG_HOME/nvim
 
 if [ ! -f $NVIM_CONFIG_DIR/autoload/plug.vim ]; then
   echo "  $XMARK vim-plug not installed"
