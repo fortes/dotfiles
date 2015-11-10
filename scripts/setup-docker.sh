aa39c51c23331ca5041145a6bdb3de9509f50a55
Filipe Fortes <accounts@fortes.com>
2015-11-10 12:18:03 -0800

    Update docker setup

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,29 +1,39 @@
 #/bin/bash
+set -ef -o pipefail
+source $HOME/dotfiles/scripts/helpers.sh
+
+if ! which apt-get > /dev/null; then
+  echo "$XMARK Setup only supported on Ubuntu systems for now"
+  exit 1
+fi
 
 # Add the Docker repo GPG key
 if ! which docker > /dev/null; then
-  echo "Docker not installed. Adding respository (requires sudo)"
+  echo "$ARROW Docker not installed. Adding respository (requires sudo)"
+  DISTRO=`lsb_release -s -c`
   sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-  sudo add-apt-repository 'deb https://apt.dockerproject.org/repo ubuntu-vivid main'
+  sudo add-apt-repository 'deb https://apt.dockerproject.org/repo ubuntu-${DISTRO} main'
+  unset DISTRO
 
-  echo "Installing Docker (requires sudo)"
+  echo "$ARROW Installing Docker (requires sudo)"
   sudo apt-get -q update
   sudo apt-get -qfuy install docker-engine
 fi
 
-echo "Checking if ufw is running (may require sudo)"
+echo "$ARROW Checking if ufw is running (may require sudo)"
 if which ufw > /dev/null && sudo ufw status | grep -qw active; then
   FWD_POLICY_STRING="DEFAULT_FORWARD_POLICY=\"ACCEPT\""
   if ! grep -qw $FWD_POLICY_STRING; then
-    echo "Setting $FWD_POLICY_STRING (requires sudo)"
+    echo "$ARROW Setting $FWD_POLICY_STRING (requires sudo)"
     sudo sed -i.bak \
       's/DEFAULT_FORWARD_POLICY="[a-zA-Z]"/'.DEFAULT_FORWARD_POLICY.'/g' \
       /etc/default/ufw
     sudo rm /etc/default/ufw.bak
   fi
 
-  echo "Allowing incoming connections on Docker port (requires sudo)"
+  echo "$ARROW Allowing incoming connections on Docker port (requires sudo)"
   sudo ufw allow 2375/tcp
+  echo "$CMARK ufw rules for docker setup"
 fi
 
-echo "Docker installed and setup"
+echo "$CMARK Docker installed and setup"
