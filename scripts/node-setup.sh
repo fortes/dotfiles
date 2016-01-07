06a3f48482896f903d9442755baf17c0cd54499c
Filipe Fortes <accounts@fortes.com>
2016-01-06 16:42:07 -0800

    Fix node setup

--- scripts/node-setup.sh
+++ scripts/node-setup.sh
@@ -10,7 +10,7 @@ else
       echo "$ARROW Adding $DISTRO nodesource PPA (requires sudo)"
       curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | \
         sudo apt-key add -
-      sudo add-apt-repository "deb https://deb.nodesource.com/node_5.x ${DISTRO,,} main"
+      sudo add-apt-repository "deb https://deb.nodesource.com/node_5.x ${VERSION,,} main"
 
       sudo apt-get update
       echo "$ARROW Installing node (requires sudo)"
