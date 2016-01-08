bf95bed147631af56092e1d80df6f5dde5e6c313
Filipe Fortes <accounts@fortes.com>
2016-01-08 10:49:38 -0800

    Update debian backports with Ubuntu note

--- scripts/debian-backports.sh
+++ scripts/debian-backports.sh
@@ -2,23 +2,26 @@
 set -ef -o pipefail
 source "$HOME/dotfiles/scripts/helpers.sh"
 
-if [ "$DISTRO" != "Debian" ]; then
+if [ "$DISTRO" = "Debian" ]; then
+  if [ "$VERSION" != "jessie" ]; then
+    echo "$XMARK Only makes sense on jessie, not $VERSION"
+    exit 1
+  fi
+
+  if ! grep -q backports /etc/apt/sources.list; then
+    echo "$XMARK Backports not in sources.list"
+    echo "  $ARROW Adding backports to in sources.list (requires sudo)"
+    sudo add-apt-repository \
+      "deb http://http.debian.net/debian jessie-backports main"
+    echo "$CMARK Backports in sources.list, updating sources"
+    sudo apt-get update
+  fi
+elif [ "$DISTRO" = "Ubuntu" ]; then
+  # Backports comes pre-configured in Ubuntu systems these days
+  true
+else
   echo "$XMARK Only supported on Debian systems"
   exit 1
 fi
 
-if [ "$VERSION" != "jessie" ]; then
-  echo "$XMARK Only makes sense on jessie, not $VERSION"
-  exit 1
-fi
-
-if ! grep -q backports /etc/apt/sources.list; then
-  echo "$XMARK Backports not in sources.list"
-  echo "  $ARROW Adding backports to in sources.list (requires sudo)"
-  sudo add-apt-repository \
-    "deb http://http.debian.net/debian jessie-backports main"
-  echo "$CMARK Backports in sources.list, updating sources"
-  sudo apt-get update
-fi
-
-echo "$CMARK Backports setup. Use 'apt-get -t jessie-backports install'"
+echo "$CMARK Backports setup. Use 'apt-get -t $VERSION-backports install'"
