2a9d7712ce92ee1a778ffb8dfbddb54e3287a8ce
Filipe Fortes <accounts@fortes.com>
2016-01-14 13:40:31 -0800

    Remove beets from mainline setup

---
+++ scripts/beets-setup.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+set -euf
+source "$HOME/dotfiles/scripts/helpers.sh"
+
+if [ "$OS" = "Linux" ]; then
+  echo "$ARROW (Re-)installing beets and dependencies (requires sudo)"
+  sudo apt-get -qfuy install mp3val
+  pip install -U beautifulsoup4 beets flask pyacoustid pylast requests
+  echo "$CMARK Everything installed"
+else
+  echo "$XMARK Unsupported OS"
+  exit 1
+fi
