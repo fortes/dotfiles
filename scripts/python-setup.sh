b09296a6ac869bec2ba8c2b68afd5cf9e4e7969c
Filipe Fortes <accounts@fortes.com>
2016-08-05 11:36:45 -0700

    Switch to python 3

--- scripts/python-setup.sh
+++ scripts/python-setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -ef -o pipefail
-source $HOME/dotfiles/scripts/helpers.sh
+source "$HOME/dotfiles/scripts/helpers.sh"
 # Make sure to pick up $VIRTUAL_ENV_DEFAULT_DIR
-source $HOME/.profile
+source "$HOME/.profile"
 
 # Create default virtualenv
-if [ ! -d $VIRTUAL_ENV_DEFAULT_DIR ]; then
+if [ ! -d "$VIRTUAL_ENV_DEFAULT_DIR" ]; then
   echo "$XMARK Default virtualenv not present"
-  mkdir -p $VIRTUAL_ENV_DEFAULT_DIR
+  mkdir -p "$VIRTUAL_ENV_DEFAULT_DIR"
   echo "$ARROW Creating default virtualenv"
-  virtualenv $VIRTUAL_ENV_DEFAULT_DIR
+  virtualenv "$VIRTUAL_ENV_DEFAULT_DIR" -p "$(which python3)"
 fi
 
 echo "$CMARK Default virtualenv present"
 
-# Always activate default virtualenv, since we install everything via pip.
-source $VIRTUAL_ENV_DEFAULT_DIR/bin/activate
+# Activate default virtualenv to install everything via pip.
+source "$VIRTUAL_ENV_DEFAULT_DIR/bin/activate"
 
 # Install python packages
 for package in $(cat $HOME/dotfiles/scripts/python-packages); do
