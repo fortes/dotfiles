15d71006fa64fd58d9602bccb41f8444f1be2a10
Filipe Fortes <accounts@fortes.com>
2016-08-18 09:10:23 -0700

    Remove more unused / stale scripts

--- scripts/sync-photo.sh
+++
@@ -1,7 +0,0 @@
-set -euo pipefail
-
-DESTINATION_DRIVE=/Volumes/backup/
-
-echo "Cleaning directory before sync"
-dot_clean /var/photo
-rsync -avzhm --progress --size-only --force --delete-after /var/photo/ $DESTINATION_DRIVE/photo
