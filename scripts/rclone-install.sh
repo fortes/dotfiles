bda306334abcd14235f75e91af7bed6f9f89a667
Filipe Fortes <accounts@fortes.com>
2016-09-06 16:15:35 -0400

    Make sure rclone install does not fail if missing local bin

--- scripts/rclone-install.sh
+++ scripts/rclone-install.sh
@@ -5,6 +5,7 @@ pushd "$TEMP_DIR" > /dev/null
 wget -q -O "$TEMP_DIR/rclone.zip" "http://downloads.rclone.org/rclone-current-linux-amd64.zip"
 unzip -j "$TEMP_DIR/rclone.zip"
 chmod +x rclone
+mkdir -p "$HOME/.local/bin"
 mv rclone "$HOME/.local/bin/."
 popd > /dev/null
 rm -rf "$TEMP_DIR"
