1430e220367abc95399e4e73954a1f1c14fa1c63
Filipe Fortes <accounts@fortes.com>
2016-11-20 12:11:30 -0800

    Install cmus from source

--- scripts/cmus-setup.sh
+++ scripts/cmus-setup.sh
@@ -4,20 +4,21 @@ source "$HOME/dotfiles/scripts/helpers.sh"
 
 # Ubuntu cmus is ancient
 if command -v apt-get > /dev/null && ! command -v cmus > /dev/null; then
-  LATEST_URL="https://github.com/cmus/cmus/archive/v2.7.1.tar.gz"
   LOCAL_DIR=$HOME/.local
-  INSTALL_DIR="$LOCAL_DIR/source/cmus"
 
   echo "$ARROW Installing build dependencies (requires sudo)"
   sudo apt-get -qqfuy install libmad0-dev libao-dev libncursesw5-dev
 
-  mkdir -p "$INSTALL_DIR"
-
-  pushd "$INSTALL_DIR" > /dev/null
-  echo "Downloading cmus 2.7.1 ..."
-  wget -q "$LATEST_URL" -O cmus.tar.gz
-  tar zxf cmus.tar.gz -C "$INSTALL_DIR" --strip-components=1
-  rm cmus.tar.gz
+  SOURCE_DIR="$LOCAL_DIR/source/cmus"
+  if [ ! -d "$SOURCE_DIR" ]; then
+    echo "$ARROW Cloning repository"
+    git clone https://github.com/cmus/cmus.git "$SOURCE_DIR"
+    pushd "$SOURCE_DIR" > /dev/null
+  else
+    pushd "$SOURCE_DIR" > /dev/null
+    echo "$ARROW Pulling repository for latest"
+    git pull
+  fi
   ./configure prefix="$LOCAL_DIR"
   make install
   echo make install
