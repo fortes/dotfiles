c718c3a19755508f24e86c514f16ccde53af909f
Filipe Fortes <accounts@fortes.com>
2016-12-15 14:17:24 -0800

    Don't regenerate locale if not needed

--- scripts/locale-gen.sh
+++ scripts/locale-gen.sh
@@ -2,7 +2,7 @@
 source "$HOME/dotfiles/scripts/helpers.sh"
 
 installAptPackagesIfMissing locales
-if locale | grep -iq posix; then
+if ! locale -a | grep -iq "en_us.utf8"; then
   echo "$ARROW Generating locale"
   sudo locale-gen en_US.UTF-8 && sudo dpkg-reconfigure locales
 fi
