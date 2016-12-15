56f067a85d765779cf27c8277804935b5fcc7ed5
Filipe Fortes <accounts@fortes.com>
2016-12-15 15:07:56 -0800

    Remove un-needed Intel NUC script

--- scripts/intel-nuc-setup.sh
+++
@@ -1,9 +0,0 @@
-# Get better audio drivers
-if [ ! -f /etc/apt/sources.list.d/ubuntu-audio-dev-alsa-daily-trusty.list ]; then
-  echo "Adding alsa-daily apt source (requires sudo)"
-  sudo add-apt-repository -y ppa:ubuntu-audio-dev/alsa-daily
-  sudo apt-get -q update
-  sudo -E apt-get -qqfuy install oem-audio-hda-daily-dkms
-
-  echo "New Audio Drivers Added. You need to reboot"
-fi
