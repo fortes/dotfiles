9702ac65645458abf192c1be1c0d9b71efc2a4ee
Filipe Fortes <accounts@fortes.com>
2016-12-16 09:14:20 -0800

    Keyboard setup tweaks

--- scripts/debian-keyboard.sh
+++ scripts/debian-keyboard.sh
@@ -9,7 +9,12 @@ fi
 
 if ! grep -q "ctrl:nocaps" /etc/default/keyboard; then
   echo "$ARROW Mapping Caps Lock to Control (requires sudo)"
-  sudo sed -i.bak 's/XKBOPTIONS=""/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard
+  sudo sed -i.bak 's/^XKBOPTIONS=""/XKBOPTIONS="altwin:left_meta_win,ctrl:nocaps"/' /etc/default/keyboard
+
+  if [ "$IS_CROUTON" == 1 ]; then
+    echo "$ARROW Using crouton keyboard (requires sudo)"
+    sudo sed -i.bak 's/^XKBMODEL=".*"$/XKBMODEL="chromebook"/' /etc/default/keyboard
+  fi
   sudo dpkg-reconfigure -phigh console-setup
 fi
 
