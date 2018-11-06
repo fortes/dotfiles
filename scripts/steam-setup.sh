5cf1ade8c4e5018a93b87e260d6323f1a503eebf
Filipe Fortes <accounts@fortes.com>
2018-11-06 18:17:53 -0500

    Steam install

---
+++ scripts/steam-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ef -o pipefail
+# shellcheck source=helpers.sh
+source "$HOME/dotfiles/scripts/helpers.sh"
+
+if ! dpkg --print-foreign-architectures | grep -q i386; then
+  echo "$XMARK i386 architecture not present"
+  echo "$ARROW Adding 32 bit (requires sudo)"
+  sudo dpkg --add-architecture i386
+  sudo apt-get -qq update
+fi
+echo "$CMARK i386 architecture present"
+
+# Need to manually accept license
+sudo apt-get install steam
