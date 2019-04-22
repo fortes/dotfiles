af57d10aeedd9ee5f7b2a1e373b38c95996a53d2
Filipe Fortes <accounts@fortes.com>
2019-04-22 12:33:49 -0400

    More background tweaks

--- stowed-files/cmus/.local/bin/cmus-album-background.sh
+++ stowed-files/cmus/.local/bin/cmus-album-background.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 PREVIOUS_FOLDER_PATH="/tmp/album-folder-path.txt"
 TMP_ART_PATH="/tmp/album-bg-blurred.png"
+DEFAULT_BG="#222222"
 
 CMUS_STATUS=$(cmus-remote -Q 2>/dev/null)
 if [[ $? != 0 ]]; then
   echo 'cmus not running, exiting'
-  hsetroot -solid "#222222"
+  hsetroot -solid "$DEFAULT_BG"
   rm "$TMP_ART_PATH" "$PREVIOUS_FOLDER_PATH"
   exit 1
 fi
 
 if grep -iq "^status stopped" <<< "$CMUS_STATUS"; then
   echo 'cmus stopped, exiting'
-  hsetroot -solid "#222222"
+  hsetroot -solid "$DEFAULT_BG"
   rm "$TMP_ART_PATH" "$PREVIOUS_FOLDER_PATH"
   exit 1
 fi
@@ -25,18 +26,19 @@ if [[ "$ALBUM_DIRPATH" != "$PREVIOUS_DIRPATH" ]]; then
 
   if [[ $? != 0 ]] || [[ -z "$ART_PATH" ]]; then
     echo 'album art file not found, exiting'
-    hsetroot -solid "#222222"
+    hsetroot -solid "$DEFAULT_BG"
     rm "$TMP_ART_PATH"
     exit 1
   fi
 
   RESOLUTION=$(xrandr | grep \* | awk '{print $1}')
-  convert "$ART_PATH" -scale 20% -scale "$RESOLUTION"\^ \
-    -modulate 100,20,100 -fill black -colorize 20% \
+  convert "$ART_PATH" -background "$DEFAULT_BG" \
+    -scale "$RESOLUTION"\^ -paint 6 \
+    -modulate 100,20,100 -fill "$DEFAULT_BG" -colorize 20% \
     "$TMP_ART_PATH"
 
   echo "setting background to $TMP_ART_PATH"
-  feh --bg-fill "$TMP_ART_PATH"
+  feh --bg-center "$TMP_ART_PATH"
 else
   echo 'already set to current album'
 fi
