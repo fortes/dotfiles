63e5bed3f21b4fc1f74d42d766c723e60559c4de
Filipe Fortes <accounts@fortes.com>
2019-04-22 12:32:55 -0400

    Simplify Node setup for Buster

--- scripts/node-setup.sh
+++ scripts/node-setup.sh
@@ -3,21 +3,6 @@ set -ef -o pipefail
 # shellcheck source=helpers.sh
 source "$HOME/dotfiles/scripts/helpers.sh"
 
-YARN_SOURCES_FILE=/etc/apt/sources.list.d/yarn.list
-if [ ! -f $YARN_SOURCES_FILE ]; then
-  echo "$XMARK Yarn not in sources.list"
-  echo "  $ARROW Adding yarn to in sources.list (requires sudo)"
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | \
-    sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | \
-    sudo tee "$YARN_SOURCES_FILE" > /dev/null
-  sudo apt-get update -qq
-fi
-echo "$CMARK Yarn in sources.list"
-
-echo "$ARROW Installing node and yarn (requires sudo)"
-installAptPackagesIfMissing nodejs yarn
-
 if ! update-alternatives --get-selections | grep -q "^node"; then
   echo "$ARROW Updating alternatives to set symlinks for nodejs to node"
   sudo update-alternatives --install \
