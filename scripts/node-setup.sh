b5a09cbec3c082fba7bce50b92e6e342758b67b2
Filipe Fortes <accounts@fortes.com>
2019-04-22 14:47:16 -0400

    Go back to old Yarn way

--- scripts/node-setup.sh
+++ scripts/node-setup.sh
@@ -3,6 +3,24 @@ set -ef -o pipefail
 # shellcheck source=helpers.sh
 source "$HOME/dotfiles/scripts/helpers.sh"
 
+# TODO: Use `yarnpkg` from system, but need to figure out how
+# to co-exist with `n`/`nvm` when not using system version,
+# may have to install locally via `yarn global add yarn`?
+YARN_SOURCES_FILE=/etc/apt/sources.list.d/yarn.list
+if [ ! -f $YARN_SOURCES_FILE ]; then
+  echo "$XMARK Yarn not in sources.list"
+  echo "  $ARROW Adding yarn to in sources.list (requires sudo)"
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | \
+    sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | \
+    sudo tee "$YARN_SOURCES_FILE" > /dev/null
+  sudo apt-get update -qq
+fi
+echo "$CMARK Yarn in sources.list"
+
+echo "$ARROW Installing yarn (requires sudo)"
+installAptPackagesIfMissing yarn
+
 if ! update-alternatives --get-selections | grep -q "^node"; then
   echo "$ARROW Updating alternatives to set symlinks for nodejs to node"
   sudo update-alternatives --install \
