a8cde15e81cf5f869befc1d60c34cae30aad20f2
Filipe Fortes <accounts@fortes.com>
2019-06-03 03:31:46 -0400

    Use system yarn

--- scripts/node-setup.sh
+++ scripts/node-setup.sh
@@ -3,30 +3,11 @@ set -ef -o pipefail
 # shellcheck source=helpers.sh
 source "$HOME/dotfiles/scripts/helpers.sh"
 
-# TODO: Use `yarnpkg` from system, but need to figure out how
-# to co-exist with `n`/`nvm` when not using system version,
-# may have to install locally via `yarn global add yarn`?
-YARN_SOURCES_FILE=/etc/apt/sources.list.d/yarn.list
-if [ ! -f $YARN_SOURCES_FILE ]; then
-  echo "$XMARK Yarn not in sources.list"
-  echo "  $ARROW Adding yarn to in sources.list (requires sudo)"
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | \
-    sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | \
-    sudo tee "$YARN_SOURCES_FILE" > /dev/null
-  sudo apt-get update -qq
+if ! update-alternatives --get-selections | grep -q "^yarn"; then
+  echo "$ARROW Updating alternative yarnpkg to yarn"
+  sudo update-alternatives --install /usr/bin/yarn yarn /usr/bin/yarnpkg 10
 fi
-echo "$CMARK Yarn in sources.list"
-
-echo "$ARROW Installing yarn (requires sudo)"
-installAptPackagesIfMissing yarn
-
-if ! update-alternatives --get-selections | grep -q "^node"; then
-  echo "$ARROW Updating alternatives to set symlinks for nodejs to node"
-  sudo update-alternatives --install \
-    /usr/bin/node node "$(command -v nodejs)" 10
-fi
-echo "$CMARK Node system alternatives set"
+echo "$CMARK Yarn system alternatives set"
 
 # Create directories for packages
 NPM_PREFIX=$HOME/.local
@@ -34,19 +15,14 @@ NPM_CACHE_DIR=$HOME/.cache/npm
 mkdir -p "$NPM_PREFIX/bin"
 mkdir -p "$NPM_CACHE_DIR"
 
-# Yarn is fast enough that we just install everything at once
-echo "$ARROW Installing global node packages"
-< "$HOME/dotfiles/scripts/node-packages" xargs yarn global add --ignore-engines
-
 NPM_BASH_COMPLETION_PATH="$HOME/.local/completions.d/npm"
 if [ ! -f "$NPM_BASH_COMPLETION_PATH" ]; then
   mkdir -p "$(dirname "$NPM_BASH_COMPLETION_PATH")"
   npm completion > "$NPM_BASH_COMPLETION_PATH"
 fi
 
-# Make sure $PATH is up to date
-source "$HOME/.profile"
-# Setup latest node/npm locally
-n stable
+# Yarn is fast enough that we just install everything at once
+echo "$ARROW Installing global node packages"
+< "$HOME/dotfiles/scripts/node-packages" xargs yarn global add --ignore-engines
 
 echo "$CMARK All node packages installed"
--- scripts/yarn-sources.sh
+++ scripts/yarn-sources.sh
@@ -7,12 +7,10 @@ YARN_SOURCES_FILE=/etc/apt/sources.list.d/yarn.list
 if [ ! -f $YARN_SOURCES_FILE ]; then
   echo "$XMARK Yarn not in sources.list"
   echo "  $ARROW Adding yarn to in sources.list (requires sudo)"
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | \
+    sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | \
-    sudo tee "$YARN_SOURCES_FILE"
+    sudo tee "$YARN_SOURCES_FILE" > /dev/null
   sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq
 fi
 echo "$CMARK Yarn in sources.list"
-
-echo "$ARROW Installing Yarn (requires sudo)"
-installAptPackagesIfMissing yarn
