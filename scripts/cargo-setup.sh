81ffd4d8f2da83c3d71e8e792028df595b695fe3
Filipe Fortes <accounts@fortes.com>
2019-12-18 12:54:06 -0800

    Try to fix Cargo building on latest Debian

--- scripts/cargo-setup.sh
+++ scripts/cargo-setup.sh
@@ -5,6 +5,9 @@ source "$HOME/dotfiles/scripts/helpers.sh"
 # Make sure to pick up path settings before install fully complete
 source "$HOME/dotfiles/stowed-files/bash/.profile"
 
+# Needed in order to be able to build with cargo
+installAptPackagesIfMissing cargo clang cmake gcc libclang-dev pkg-config
+
 echo "$ARROW Installing/upgrading cargo packages"
 for PACKAGE in $(cat $HOME/dotfiles/scripts/cargo-packages); do
   if ! cargo install --list | grep -q " $PACKAGE"; then
