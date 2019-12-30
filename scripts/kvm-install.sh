93fe94512793a3a0c07e8a67c9e31d9248d48a86
Filipe Fortes <accounts@fortes.com>
2019-12-30 12:56:30 -0500

    More KVM setup tweaks

--- scripts/kvm-install.sh
+++ scripts/kvm-install.sh
@@ -5,6 +5,13 @@ source "$HOME/dotfiles/scripts/helpers.sh"
 
 echo "$ARROW Installing KVM stuff (requires sudo)"
 sudo apt-get install --no-install-recommends -qqfuy \
-  qemu-kvm libvirt-clients libvirt-daemon-system virtinst
+  qemu-kvm libvirt-clients libvirt-daemon-system virtinst dnsmasq-base
+
+# Can hit all sorts of polkit errors if you don't do this
+echo "$ARROW Add user to libvirt group (requires sudo)"
+sudo usermod -a -G libvirt "$(whoami)"
+
+# TODO: Make sure network autostarts
+# virsh net-autostart default
 
 echo "$CMARK KVM installed"
