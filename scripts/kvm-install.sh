e947e0e3882f8e5fb4d276c74a5060cbdd850aba
Filipe Fortes <accounts@fortes.com>
2019-12-30 12:46:05 -0500

    Install virtinst

--- scripts/kvm-install.sh
+++ scripts/kvm-install.sh
@@ -5,6 +5,6 @@ source "$HOME/dotfiles/scripts/helpers.sh"
 
 echo "$ARROW Installing KVM stuff (requires sudo)"
 sudo apt-get install --no-install-recommends -qqfuy \
-  qemu-kvm libvirt-clients libvirt-daemon-system
+  qemu-kvm libvirt-clients libvirt-daemon-system virtinst
 
 echo "$CMARK KVM installed"
