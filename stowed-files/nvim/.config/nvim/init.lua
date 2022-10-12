939159f36c654879ce12da6760f80e08a300a461
Filipe Fortes <accounts@fortes.com>
2022-10-12 09:46:25 -0400

    Make OSC52 copy work from neovim in tmux

--- stowed-files/nvim/.config/nvim/init.lua
+++ stowed-files/nvim/.config/nvim/init.lua
@@ -368,12 +368,13 @@ require('packer').startup(function(use)
   -- Honor `.editorconfig`
   use {'editorconfig/editorconfig-vim'}
 
-  -- Copy to system via OSC52
+  -- Copy to system via OSC52 via <leader>c
   use {
-    'ojroques/vim-oscyank',
+    'ojroques/nvim-osc52',
     config = function()
-      vim.keymap.set('v', '<leader>y', ':OSCYank<cr>',
-        {noremap=true, silent=true})
+      vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr=true})
+      vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap=true})
+      vim.keymap.set('x', '<leader>c', require('osc52').copy_visual)
     end
   }
 
