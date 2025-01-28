69e9ce1716d3c1bcb76579a4b8ee5792c453d71f
Filipe Fortes <accounts@fortes.com>
2025-01-28 15:10:35 -0500

    Remove overlapping copy map

--- stowed-files/nvim/.config/nvim/init.lua
+++ stowed-files/nvim/.config/nvim/init.lua
@@ -479,7 +479,7 @@ require("lazy").setup({
   {
     'ojroques/nvim-osc52',
     config = function()
-      vim.keymap.set('n', '<leader>c', require('osc52').copy_operator, {expr=true})
+      vim.keymap.set('n', '<leader>cy', require('osc52').copy_operator, {expr=true})
       vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap=true})
       vim.keymap.set('x', '<leader>c', require('osc52').copy_visual)
     end
