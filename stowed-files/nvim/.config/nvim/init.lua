cb047298d2de8037db8bc133fec3c831a23ae3be
Filipe Fortes <accounts@fortes.com>
2025-03-14 16:14:04 -0400

    Remove OSC52 plugin, now that it is built-in

--- stowed-files/nvim/.config/nvim/init.lua
+++ stowed-files/nvim/.config/nvim/init.lua
@@ -505,16 +505,6 @@ require("lazy").setup({
     end
   },
 
-  -- Copy to system via OSC52 via <leader>c
-  {
-    'ojroques/nvim-osc52',
-    config = function()
-      vim.keymap.set('n', '<leader>cy', require('osc52').copy_operator, {expr=true})
-      vim.keymap.set('n', '<leader>cc', '<leader>c_', {remap=true})
-      vim.keymap.set('x', '<leader>c', require('osc52').copy_visual)
-    end
-  },
-
   -- Display available key bindings, marks, registers, etc
   {
     'folke/which-key.nvim',
