8266a662f5934ad7538b30ff0c7a7be60cab566a
Filipe Fortes <accounts@fortes.com>
2025-08-07 20:20:14 -0400

    Clean up WezTerm config
    - Move color schemes into varaibles
    - Fix typo in color scheme names ("base 16" -> "base16")
    - Execute color change script as background child process instead of new tab

--- stowed-files/wezterm/.config/wezterm/wezterm.lua
+++ stowed-files/wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,10 @@
 local wezterm = require 'wezterm';
 local config = wezterm.config_builder();
 
+-- Color scheme names
+local DARK_COLOR_SCHEME_NAME = 'Default Dark (base16)'
+local LIGHT_COLOR_SCHEME_NAME = 'Default Light (base16)'
+
 -- WezTerm default keys are pretty nice
 --
 -- Paste: Ctrl+Shift+V
@@ -12,46 +16,48 @@ local config = wezterm.config_builder();
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.color_scheme = 'Default Dark (base16)'
+config.color_scheme = DARK_COLOR_SCHEME_NAME
 
-config.font = wezterm.font("JetBrains Mono", { weight = "Light" })
+config.font = wezterm.font_with_fallback({
+  { family = "Google Sans Code", weight = 400 },
+  -- Google Sans currently only installed on Mac, fallback for Linux
+  { family = "JetBrains Mono",   weight = 400 },
+})
 
 config.window_padding = {
-  left = 0,
-  right = 0,
+  left = 4,
+  right = 4,
   top = 0,
   bottom = 0,
 }
 
--- Toggle colorscheme with Ctrl+Shift+L
 wezterm.on('toggle-color-scheme', function(window, pane)
   local overrides = window:get_config_overrides() or {}
-  if overrides.color_scheme == 'Default Dark (base 16)' then
-    overrides.color_scheme = 'Default Light (base 16)'
+  local effective = window:effective_config() or {}
+  local current_scheme = overrides.color_scheme or effective.color_scheme
+  if current_scheme == DARK_COLOR_SCHEME_NAME then
+    overrides.color_scheme = LIGHT_COLOR_SCHEME_NAME
   else
-    overrides.color_scheme = 'Default Dark (base 16)'
+    overrides.color_scheme = DARK_COLOR_SCHEME_NAME
   end
   window:set_config_overrides(overrides)
+
+  wezterm.background_child_process({
+    'bash',
+    '-lc',
+    [[. ~/.profile && . ~/.bashrc && \
+echo -n "[$(date "+%Y-%m-%d %H:%M:%S")] " >> /tmp/wezterm-toggle.log && \
+~/dotfiles/stowed-files/bash/.local/bin/toggle-color-scheme >> /tmp/wezterm-toggle.log 2>&1]]
+  })
 end)
 
 config.keys = {
+  -- Toggle colorscheme with Ctrl+Shift+L
   {
     key = "L",
     mods = "CTRL|SHIFT",
     action = wezterm.action.Multiple {
       wezterm.action.EmitEvent "toggle-color-scheme",
-      -- Spawn shell to run `toggle-color-scheme`
-      wezterm.action.SpawnCommandInNewTab {
-        args = {
-          'bash',
-          '-c',
-          [[\
-. ~/.profile && . ~/.bashrc && \
-  echo -n "[$(date "+%Y-%m-%d %H:%M:%S")] " >> /tmp/wezterm-toggle.log && \
-  ~/dotfiles/stowed-files/bash/.local/bin/toggle-color-scheme >> /tmp/wezterm-toggle.log 2>&1
-          ]]
-        }
-      },
     },
   },
 }
